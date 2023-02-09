648948652b4d768d4563acbbe00409c087b4de7a
Aman Gupta <amangupta1804@gmail.com>
2023-02-10 03:29:44 +0530

    movie app

---
+++ src/component/Actor.js
@@ -0,0 +1,58 @@
+import { Link,useParams} from "react-router-dom";
+import React, {useEffect, useState } from 'react';
+import axios from 'axios';
+
+function Actor() {
+
+const {actorId} = useParams();
+const [data, setdata] = useState();
+const url=`https://imdb-api.com/en/API/Name/k_nt4ls040/${actorId}`;
+
+
+
+
+useEffect(() => {
+
+    function fun1() {
+    const response =  axios.get(url)
+
+    setdata(response.data)
+  }
+
+  fun1();
+
+},[url])
+
+
+  return (
+    <div>
+    <div className='actorCircle'>
+        <img src={data?.image} alt='actor' />
+    </div>
+    <div className='actorDetails'>
+        <h1>{data?.name}</h1>
+        <h5>{data?.role}</h5>
+        <p>{data?.summary}</p>
+        <h2>{data?.awards}</h2>
+    </div>
+    <div className='actorMovies'>
+      {
+        data?.knownFor?.map((movie)=>(
+          <div className='actorCard'>
+            <Link to={`/movie/${movie.id}`}>
+            <img src={movie?.movie} alt='movie'/>
+            <h3>{movie?.title}</h3>
+            </Link>
+        </div>
+        ))
+
+      }
+        
+           
+       
+    </div>
+</div>
+  )
+}
+
+export default Actor
